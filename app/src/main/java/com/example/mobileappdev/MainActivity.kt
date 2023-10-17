2033ba577f14a6adbe2162dab78bd916c61a1277
fwvu <bryanfvu@gmail.com>
2023-10-17 18:41:00 +1100

    added maindashboard mainactivity points to main dashboard (change later to login??) bottom nav bar added top header added working

--- app/src/main/java/com/example/mobileappdev/MainActivity.kt
+++ app/src/main/java/com/example/mobileappdev/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mobileappdev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var button: AppCompatButton? = null
+
+    private val startApp by lazy {
+        Intent(this, MainDashboard::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.startButton) as? AppCompatButton
+
+        button?.setOnClickListener {
+            startActivity(startApp)
+        }
+
     }
 }
